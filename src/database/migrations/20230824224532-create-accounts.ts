3d4f6cb6ea29e0fe5e0ed6a3cc9ac484da257203
Bruno Alvim Duarte <brunoalvimduarte@gmail.com>
2023-08-25 15:07:25 -0300

    Criando a migration pra criar a tabela accounts, que armazenará as entidades de Account

---
+++ src/database/migrations/20230824224532-create-accounts.ts
@@ -0,0 +1,39 @@
+import { QueryInterface, DataTypes } from "sequelize";
+
+
+export default {
+  async up (queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
+    await queryInterface.createTable("Accounts", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      CPF: {
+        type: Sequelize.STRING(11),
+        allowNull: true,
+      },
+      CNPJ: {
+        type: Sequelize.STRING(14),
+        allowNull: true,
+      }
+    })
+  },
+
+  async down (queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
+    await queryInterface.dropTable("Accounts");
+  }
+};
