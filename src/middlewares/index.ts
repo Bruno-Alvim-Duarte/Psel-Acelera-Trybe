0f494bd7436db551afc4ab21be8bd11c56844b5a
Bruno Alvim Duarte <brunoalvimduarte@gmail.com>
2023-08-25 23:58:51 -0300

    criando o middleware para rota de atualizar uma Account

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -19,4 +19,18 @@ export default class Middlewares {
 
     next();
   }
+
+  static validateUpdateAccount(req: Request, res: Response, next: NextFunction) {
+    const { name, email, password, CPF, CNPJ } = req.body;
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: 'Name, email and password is required' });
+    }
+
+    if (CPF || CNPJ) {
+      return res.status(400).json({ message: 'CPF or CNPJ cannot be updated' });
+    }
+
+    next();
+  }
 }
