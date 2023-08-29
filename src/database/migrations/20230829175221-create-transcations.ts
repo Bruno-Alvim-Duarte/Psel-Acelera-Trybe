165f072e2aca547f5440f30230ac399854795ef5
Bruno Alvim Duarte <brunoalvimduarte@gmail.com>
2023-08-29 15:14:44 -0300

    Criando a model de Transaction, que representára uma entidade de transação, e o criando a migration pra criar essa tabela no banco de dados

---
+++ src/database/migrations/20230829175221-create-transcations.ts
@@ -0,0 +1,35 @@
+import { QueryInterface, DataTypes } from "sequelize";
+
+
+export default {
+  async up (queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
+    await queryInterface.createTable("Transactions", {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      account_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      date: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      value: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+      cashback: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false,
+      },
+    })
+  },
+
+  async down (queryInterface: QueryInterface, Sequelize: typeof DataTypes) {
+    await queryInterface.dropTable("Transactions");
+  }
+};
---
+++ src/database/models/Transaction.ts
@@ -0,0 +1,47 @@
+import { CreationOptional, DataTypes,
+  InferAttributes, InferCreationAttributes, Model } from 'sequelize';
+import sequelize from '.';
+import Account from './Account';
+
+class Transaction extends Model<InferCreationAttributes<Transaction>,
+InferAttributes<Transaction>> {
+  declare id: CreationOptional<number>;
+  declare accountId: number;
+  declare date: Date;
+  declare value: number;
+  declare cashback: number;
+}
+
+Transaction.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  accountId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+  },
+  value: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  cashback: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+}, {
+  sequelize,
+  tableName: 'Transactions',
+  timestamps: false,
+  underscored: true,
+});
+
+Account.hasMany(Transaction, { foreignKey: 'accountId', as: 'transactions' });
+
+Transaction.belongsTo(Account, { foreignKey: 'accountId', as: 'account' });
